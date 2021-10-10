e34d93c6dcaeb776487324633c8fcd3681baf500
akihiro-f <akihirobluesharks@gmail.com>
2021-10-11 00:45:10 +0900

    top 作成

--- dist/index.php
+++ dist/index.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>portfolio</title>
   <script src="https://kit.fontawesome.com/1af2190f68.js" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
   <link rel="stylesheet" href="./css/reset.css" media="print" onload="this.media='all'">
   <link rel="stylesheet" href="./css/style.css">
+  <title>portfolio</title>
 </head>
 
 <body>
@@ -1145,16 +1145,16 @@
       </defs>
       <a href="https://storyset.com/work">Illustration by Freepik Storyset</a>
     </svg>
-    <div class="s_profile_desc">
-      <h2 class="s_profile_desc_h2 anime">hello world.</h2>
-      <h3 class="s_profile_desc_h3 anime">My name is Akihiro Fukabe.</h3>
-      <div class='s_profile_desc_img-wrap'><img class="anime" src='./img/icon.jpg' loading="lazy" alt=''></div>
-      <ul class="s_profile_desc_list anime">
+    <div class="s_profile_desc anime">
+      <h2 class="s_profile_desc_h2">hello world.</h2>
+      <h3 class="s_profile_desc_h3">My name is Akihiro Fukabe.</h3>
+      <div class='s_profile_desc_img-wrap'><img src='./img/icon.jpg' loading="lazy" alt=''></div>
+      <ul class="s_profile_desc_list">
         <li class="s_profile_desc_list-item">名前：深辺　明寛</li>
         <li class="s_profile_desc_list-item">出身：静岡県（神奈川県在中）</li>
         <li class="s_profile_desc_list-item">趣味：水泳、釣り、DIY</li>
       </ul>
-      <a class="s_profile_link" href="./profile/"><button class="anime">profile</button></a>
+      <a class="s_profile_link" href="./profile/"><button>profile</button></a>
     </div>
     <svg class="s_profile_svg anime" id="freepik_stories-developer-activity" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs">
       <g id="freepik--Floor--inject-171" class="animable" style="transform-origin: 250px 345.35px;">
@@ -2923,57 +2923,61 @@
     </svg>
   </section>
   <section id="works" class="s_works">
-    <h2 class="s_works_h2 anime">works</h2>
-    <article class="s_works_article amore anime">
-      <a href="./amore/">
-        <div class="s_works_article_text">
-          <h3 class="s_works_article_text_h2">LPサイト（レストラン）</h3>
-          <p class="s_works_article_text_caption">[Design,HTML5,CSS3(Sass),<br>JavaScript(native)]</p>
-        </div>
-        <picture class='s_works_article_img-wrap'>
-          <source srcset="./img/amore/amore-sp.jpg" media="(max-width:559px)" />
-          <img src='./img/amore/amore1.jpg' loading="lazy" alt=''>
-        </picture>
-      </a>
-    </article>
-    <article class="s_works_article igarashi-education anime">
-      <a href="./igarashi-education">
-        <div class="s_works_article_text">
-          <h3 class="s_works_article_text_h2">企業サイト（教育）</h3>
-          <p class="s_works_article_text_caption">[HTML5,CSS3(Sass),<br>jQuery]</p>
-        </div>
-        <picture class='s_works_article_img-wrap'>
-          <source srcset="./img/igarashi-education/igarashi-education1.jpg" media="(max-width:559px)" />
-          <img src='./img/igarashi-education/igarashi-education2.jpg' loading="lazy" alt=''>
-        </picture>
-      </a>
-    </article>
-    <article class="s_works_article imaginary anime">
-      <a href="./imaginary-online/">
-        <div class="s_works_article_text">
-          <h3 class="s_works_article_text_h2">ECサイト（アパレル）</h3>
-          <p class="s_works_article_text_caption">[HTML5,CSS3(Sass),<br>JavaScript(native)]</p>
-        </div>
-        <picture class='s_works_article_img-wrap'>
-          <source srcset="./img/imaginary-online/imaginary-online-sp.jpg" media="(max-width:559px)" />
-          <img src='./img/imaginary-online/imaginary1.jpg' loading="lazy" alt=''>
-        </picture>
-      </a>
-    </article>
-    <article class="s_works_article kakigokoro anime">
-      <a href="./kakigokoro/">
-        <div class="s_works_article_text">
-          <h3 class="s_works_article_text_h2">書道家　ホームページ</h3>
-          <p class="s_works_article_text_caption">[Planning, Design,HTML5,<br>CSS3(Sass),jQuery,PHP]</p>
-        </div>
-        <picture class='s_works_article_img-wrap'>
-          <source srcset="./img/kakigokoro/kakigokoro-sp.jpg" media="(max-width:559px)" />
-          <img src='./img/kakigokoro/kakigokoro1.jpg' loading="lazy" alt=''>
-        </picture>
-      </a>
-    </article>
+    <div class="inner">
+      <h2 class="s_works_h2">works</h2>
+      <div class="wrap">
+        <article class="s_works_article amore anime">
+          <a href="./amore/">
+            <div class="s_works_article_text">
+              <h3 class="s_works_article_text_h2">LPサイト（レストラン）</h3>
+              <p class="s_works_article_text_caption">[Design,HTML5,<br>CSS3(Sass),JavaScript(native)]</p>
+            </div>
+            <picture class='s_works_article_img-wrap'>
+              <source srcset="./img/amore/amore-sp.jpg" media="(max-width:559px)" />
+              <img src='./img/amore/amore1.jpg' loading="lazy" alt=''>
+            </picture>
+          </a>
+        </article>
+        <article class="s_works_article igarashi-education anime">
+          <a href="./igarashi-education">
+            <div class="s_works_article_text">
+              <h3 class="s_works_article_text_h2">企業サイト（教育）</h3>
+              <p class="s_works_article_text_caption">[HTML5,CSS3(Sass),<br>jQuery]</p>
+            </div>
+            <picture class='s_works_article_img-wrap'>
+              <source srcset="./img/igarashi-education/igarashi-education1.jpg" media="(max-width:559px)" />
+              <img src='./img/igarashi-education/igarashi-education2.jpg' loading="lazy" alt=''>
+            </picture>
+          </a>
+        </article>
+        <article class="s_works_article imaginary anime">
+          <a href="./imaginary-online/">
+            <div class="s_works_article_text">
+              <h3 class="s_works_article_text_h2">ECサイト（アパレル）</h3>
+              <p class="s_works_article_text_caption">[HTML5,CSS3(Sass),JavaScript(native)]</p>
+            </div>
+            <picture class='s_works_article_img-wrap'>
+              <source srcset="./img/imaginary-online/imaginary-online-sp.jpg" media="(max-width:559px)" />
+              <img src='./img/imaginary-online/imaginary1.jpg' loading="lazy" alt=''>
+            </picture>
+          </a>
+        </article>
+        <article class="s_works_article kakigokoro anime">
+          <a href="./kakigokoro/">
+            <div class="s_works_article_text">
+              <h3 class="s_works_article_text_h2">書道家　ホームページ</h3>
+              <p class="s_works_article_text_caption">[Planning,Design,HTML5,<br>CSS3(Sass),jQuery,PHP]</p>
+            </div>
+            <picture class='s_works_article_img-wrap'>
+              <source srcset="./img/kakigokoro/kakigokoro-sp.jpg" media="(max-width:559px)" />
+              <img src='./img/kakigokoro/kakigokoro1.jpg' loading="lazy" alt=''>
+            </picture>
+          </a>
+        </article>
+      </div>
+    </div>
   </section>
-  <footer class="anime">
+  <footer class="footer">
     <small>
       Copyright &copy; fukabeakihiro.com All Rights Reserved
     </small>
--- dist/js/base.js
+++ dist/js/base.js
@@ -8,9 +8,9 @@ function createEle() {
         <div class="square"></div>
         <div class="square"></div>
       </div>
-    </div>`
-  document.body.insertAdjacentHTML('beforeend', spinner);
-};
+    </div>`;
+  document.body.insertAdjacentHTML("beforeend", spinner);
+}
 
 const scrollEvent = function () {
   window.addEventListener("scroll", function () {
@@ -29,45 +29,44 @@ const scrollEvent = function () {
 };
 
 // データをセット
-sessionStorage.setItem('key', 'value');
-sessionStorage.getItem('key');
+sessionStorage.setItem("key", "value");
+sessionStorage.getItem("key");
 
 //処理
 scrollEvent();
 window.onload = function () {
   createEle();
-  const spinner = document.getElementById('loading');
+  const spinner = document.getElementById("loading");
   let scrollEle = document.getElementsByClassName("anime-load");
-  console.log(spinner);
 
-  if (sessionStorage.getItem('access')) {
-    spinner.classList.add('loaded');
+  if (sessionStorage.getItem("access")) {
+    spinner.classList.add("loaded");
   } else {
     setTimeout(function () {
-      spinner.classList.add('loaded');
-      sessionStorage.setItem('access', 0);
+      spinner.classList.add("loaded");
+      sessionStorage.setItem("access", 0);
     }, 2000);
   }
 
   for (let i = 0; i < scrollEle.length; i++) {
     scrollEle[i].classList.add("animated");
   }
-}
+};
 
 //カーソルストーカー
 const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
 for (let i = 0; i < smoothScrollTrigger.length; i++) {
-  smoothScrollTrigger[i].addEventListener('click', (e) => {
+  smoothScrollTrigger[i].addEventListener("click", (e) => {
     e.preventDefault();
-    let href = smoothScrollTrigger[i].getAttribute('href');
-    let targetElement = document.getElementById(href.replace('#', ''));
+    let href = smoothScrollTrigger[i].getAttribute("href");
+    let targetElement = document.getElementById(href.replace("#", ""));
     const rect = targetElement.getBoundingClientRect().top;
     const offset = window.pageYOffset;
     const gap = 60;
     const target = rect + offset - gap;
     window.scrollTo({
       top: target,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   });
 }
@@ -78,23 +77,23 @@ class Cursor {
     <div class="js-cursor">
       <div class="js-cursor__main"></div>
       <div class="js-cursor__option"></div>
-    </div>`
-    document.body.insertAdjacentHTML('beforeend', el);
+    </div>`;
+    document.body.insertAdjacentHTML("beforeend", el);
 
-    this.wrap_el = document.querySelector('.js-cursor');
-    this.main_el = document.querySelector('.js-cursor__main');
-    this.option_el = document.querySelector('.js-cursor__option');
+    this.wrap_el = document.querySelector(".js-cursor");
+    this.main_el = document.querySelector(".js-cursor__main");
+    this.option_el = document.querySelector(".js-cursor__option");
 
     this.position = {
       mouseX: 0,
       mouseY: 0,
       currentX: 0,
-      currentY: 0
-    }
+      currentY: 0,
+    };
     this.eventStatus = {
       click: false,
-      hover: false
-    }
+      hover: false,
+    };
   }
 
   init() {
@@ -104,91 +103,97 @@ class Cursor {
 
   attachEvent() {
     //カーソルの位置を取得
-    document.addEventListener('mousemove', (e) => {
+    document.addEventListener("mousemove", (e) => {
       this.position.mouseX = e.clientX;
       this.position.mouseY = e.clientY;
-      this.wrap_el.classList.add('is-moved');
+      this.wrap_el.classList.add("is-moved");
     });
 
     //画面外判定
-    document.body.addEventListener("mouseleave", () => {
-      this.wrap_el.classList.add('is-outside');
-    }, false);
-    document.body.addEventListener("mouseenter", () => {
-      this.wrap_el.classList.remove('is-outside');
-    }, false);
+    document.body.addEventListener(
+      "mouseleave",
+      () => {
+        this.wrap_el.classList.add("is-outside");
+      },
+      false
+    );
+    document.body.addEventListener(
+      "mouseenter",
+      () => {
+        this.wrap_el.classList.remove("is-outside");
+      },
+      false
+    );
 
     //クリック判定
-    document.addEventListener('mousedown', (e) => {
+    document.addEventListener("mousedown", (e) => {
       this.eventStatus.click = true;
-    })
-    document.addEventListener('mouseup', (e) => {
+    });
+    document.addEventListener("mouseup", (e) => {
       this.eventStatus.click = false;
-    })
+    });
 
     // aタグのホバー判定
     // 監視ターゲットの取得
     const body = document.body;
     // オブザーバーの作成
-    const observer = new MutationObserver(records => {
-      let link = document.querySelectorAll('a');
+    const observer = new MutationObserver((records) => {
+      let link = document.querySelectorAll("a");
       for (const target of link) {
-        target.addEventListener('mouseenter', (e) => {
+        target.addEventListener("mouseenter", (e) => {
           this.eventStatus.hover = true;
-          this.wrap_el.classList.add('is-hover');
-        })
-        target.addEventListener('mouseleave', (e) => {
+          this.wrap_el.classList.add("is-hover");
+        });
+        target.addEventListener("mouseleave", (e) => {
           this.eventStatus.hover = false;
-          this.wrap_el.classList.remove('is-hover');
-        })
+          this.wrap_el.classList.remove("is-hover");
+        });
       }
-    })
+    });
     // 監視の開始
     observer.observe(body, {
-      childList: true
-    })
+      childList: true,
+    });
   }
 
   tween() {
-    TweenMax.to({}, .001, {
+    TweenMax.to({}, 0.001, {
       repeat: -1,
       onRepeat: () => {
-
         //減速処理
-        this.position.currentX += (this.position.mouseX - this.position.currentX) * 0.5;
-        this.position.currentY += (this.position.mouseY - this.position.currentY) * 0.5;
+        this.position.currentX +=
+          (this.position.mouseX - this.position.currentX) * 0.5;
+        this.position.currentY +=
+          (this.position.mouseY - this.position.currentY) * 0.5;
 
-        TweenMax.set(this.main_el,
-          {
-            css: {
-              x: this.position.currentX - 5,
-              y: this.position.currentY - 5
-            }
-          });
-        TweenMax.to(this.option_el, 0.3,
-          {
-            css: {
-              x: this.position.currentX - 20,
-              y: this.position.currentY - 20,
-              scale: this.scale(this.eventStatus)
-            }
-          });
-      }
+        TweenMax.set(this.main_el, {
+          css: {
+            x: this.position.currentX - 5,
+            y: this.position.currentY - 5,
+          },
+        });
+        TweenMax.to(this.option_el, 0.3, {
+          css: {
+            x: this.position.currentX - 20,
+            y: this.position.currentY - 20,
+            scale: this.scale(this.eventStatus),
+          },
+        });
+      },
     });
   }
 
   scale(v) {
     if (v.hover == true && v.click == false) {
-      return 1.6
+      return 1.6;
     } else if (v.hover == false && v.click == true) {
-      return 0.6
+      return 0.6;
     } else if (v.hover == true && v.click == true) {
-      return 0.6
+      return 0.6;
     } else {
-      return 1
+      return 1;
     }
   }
-
 }
 
 let cursor = new Cursor();
